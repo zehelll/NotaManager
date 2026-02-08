d24d36ca15edad7ed4a74a8465850d25624457da
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2026-02-08 11:49:00 -0300

    Update concluir_tarefa, update_exportar_tarefas

--- ListadeTarefas/modules/concluir_tarefa.sh
+++ ListadeTarefas/modules/concluir_tarefa.sh
@@ -2,6 +2,6 @@ concluir_tarefa() {
     listar_tarefas
     read -p "Digite o número da tarefa concluída: " num
 
-    sed -i "${num}s/^\[ \]/[X]/" data/tarefas.txt
+    sed -i "${num}s/^\[ \]/[X]/" "$TASK_FILE"
     echo "Tarefa marcada como concluída!"
 }
--- ListadeTarefas/modules/exportar_tarefas.sh
+++ ListadeTarefas/modules/exportar_tarefas.sh
@@ -1,5 +1,5 @@
 exportar_tarefas() {
     read -p "Nome do arquivo para exportação: " arquivo
-    cp data/tarefas.txt "$arquivo"
+    cp "$TASK_FILE" "$arquivo"
     echo "Tarefas exportadas para $arquivo"
 }
